dbfe540d94f2cf85bb6e18affff0a3ec8440c0b6
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2025-02-03 22:17:52 -0600

    primero de muchos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+//import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 const yesButton = document.getElementById('yesButton');
 const noButton = document.getElementById('noButton');
 const imageDisplay = document.getElementById('imageDisplay');
@@ -16,12 +16,30 @@ const imagePaths = [
   "./images/image4.gif",
   "./images/image5.gif",
   "./images/image6.gif",
-  "./images/image7.gif"
+  "./images/image7.gif",
+  "./images/image8.gif",
+  "./images/image9.gif",
+  "./images/image10.gif",
+  "./images/image11.gif",
+  "./images/image12.gif"
 ];
 
 noButton.addEventListener('click', function() {
-  if (noClickCount < 5) {
+  if (noClickCount < 9) {
     noClickCount++;
+    // Obtener el ancho y alto de la ventana
+    let maxX = window.innerWidth - buttonWidth;
+    let maxY = window.innerHeight - buttonHeight;
+
+    // Generar posiciones aleatorias dentro de la ventana
+    let randomX = Math.floor(Math.random() * maxX);
+    let randomY = Math.floor(Math.random() * maxY);
+
+    // Aplicar nuevas posiciones
+    noButton.style.position = 'absolute'; // Asegurar que el botón puede moverse libremente
+    noButton.style.left = `${randomX}px`;
+    noButton.style.top = `${randomY}px`;
+
     imageDisplay.src = imagePaths[noClickCount];
     buttonHeight += 35; // Increase height by 5px on each click
     buttonWidth += 35;
@@ -29,15 +47,19 @@ noButton.addEventListener('click', function() {
     yesButton.style.height = `${buttonHeight}px`; // Update button height
     yesButton.style.width = `${buttonWidth}px`;
     yesButton.style.fontSize = `${fontSize}px`; // Update font size
-    if (noClickCount < 6) {
-      noButton.textContent = ["No", "Are you sure?", "Pookie please", "Don't do this to me :(", "You're breaking my heart", "I'm gonna cry..."][noClickCount];
+    if (noClickCount < 10) {
+      noButton.textContent = ["No", "Are you sure?", "Pookie please", "di que sii :(", "Rompes mi corazón", "vo a llorar...","porfavooor","y te quiero mas que ayer","di que siiiiiii", "te dare muchos besos"][noClickCount];
+      if (noClickCount >= 9) {
+        noButton.style.display = 'none'; // Hide the button after 10 clicks
+      }
     }
   }
 });
 
 yesButton.addEventListener('click', () => {
-  imageDisplay.src = './images/image7.gif'; // Change to image7.gif
-  valentineQuestion.textContent = "Yayyy!! :3"; // Change the question text
+  imageDisplay.src = './images/image13.gif';
+  //imageDisplay.src = './images/image7.gif'; // Change to image7.gif
+  valentineQuestion.textContent = "Me Encantas!! :3"; // Change the question text
   responseButtons.style.display = 'none'; // Hide both buttons
-  confetti(); // Trigger confetti animation
+  //confetti(); // Trigger confetti animation
 });
