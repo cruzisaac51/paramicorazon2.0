f076941d4587f491808c8f19d24e39b69220c488
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2025-02-26 15:12:51 -0600

    invitacion

--- js/scriptf1.js
+++ js/scriptf1.js
@@ -51,5 +51,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.querySelector(".videoF1 video");
+    const placeholder = document.querySelector(".video-placeholder");
+
+    video.addEventListener("canplay", function () {
+        placeholder.style.display = "none"; // Ocultar imagen
+        video.style.display = "block"; // Mostrar video
+    });
+});
+
+document.getElementById("yesButton").addEventListener("click", function () {
+    document.getElementById("invitacion").style.display = "none"; // Oculta la invitación
+    document.getElementById("contenido").style.display = "block"; // Muestra la página
+});
+
+document.getElementById("noButton").addEventListener("click", function () {
+    alert("Está bien, te esperaré cuando quieras 💔");
+    window.location.href = "https://www.google.com/search?sca_esv=885f4de560f195c7&rlz=1C1CHBD_esMX1099MX1099&sxsrf=AHTn8zq03Zj-Pi8AX_lKnWBsqc6v9upJJQ:1740604284618&q=sad+images&udm=2&fbs=ABzOT_BnMAgCWdhr5zilP5f1cnRvK9uZj3HA_MTJAA6lXR8yQElaIApxtef1-RKg2CcwxXYFPnQ3n4nbwZZO6RCmbc-sT1H_aKpp_RnGJowF9zN6tkTCWdfQPJ60Sf3GPKs8TDK7WpavUdY7QeLc1RZarkpnsxMsOEidpdYA4Y0GwEKdA1xCczwFIH1mPoOW5CW0Z3D8rKZFw4gcbxo7OomUxX-sByITCw&sa=X&sqi=2&ved=2ahUKEwiN59CgoOKLAxWJLtAFHZ1PIeYQtKgLegQIDhAB&biw=1920&bih=911&dpr=1"; // Puedes cambiar la URL o hacer otra acción
+});
+
 // Ejecutar la función al cargar la página
 fetchNextRace();
